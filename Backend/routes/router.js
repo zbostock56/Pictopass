9e59b548793b09c267745bf5f2e1cc555e714ff7
Jack-Saysana <saysanabjack@gmail.com>
2021-09-18 11:29:38 -0400

    Updated File Input API

--- Backend/routes/router.js
+++ Backend/routes/router.js
@@ -7,18 +7,28 @@ const router = express.Router();
         img - offscreen html img
         dataUrl - image data string
 */
-router.post("/newFile", (req, res) => {
+router.post("/newImage", (req, res) => {
     let canvas = req.body.canvas;
     let image = req.body.image;
     const imgData = req.body.dataUrl;
+    let pixData = []
 
     image.src = imgData;
     canvas.width = img.width;
     canvas.height = img.height;
     canvas.getContext("2d").drawImage(image, 0, 0, canvas.width, canvas.height);
     canvas.addEventListener("load", () => {
-        //Generate Pixel data
+        const imageData = canvas.getContext("2d").getImageData(0, 0, canvas.width, canvas.height);
+        for(let i = 0; i < imageData.length / 4; i++){
+            pixData.push([0, 0, 0, 0]);
+        }
+        for(let j = 0; j < pixData.length; j++){
+            for(let k = 0; k < 4; k++){
+                pixData[j][k] = imageData[((4 * j) + k)];
+            }
+        }
     });
+    res.send(pixData);
 });
 
 router.post("/test", (req, res) => {
