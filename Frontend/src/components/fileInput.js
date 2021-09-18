da21f76b8f95b61b39cdcb6baa2661ba9a22549f
zbostock56 <50964685+zbostock56@users.noreply.github.com>
2021-09-18 11:28:30 -0400

    added file input

---
+++ Frontend/src/components/fileInput.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+//Handle file submittions to be pushed to the backend API
+
+export default class fileInput extends React.Component {
+
+    constructor(props){
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A file was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render(){
+      return(
+          <form onSubmit={this.handleSubmit}>
+              <label>
+                  Input file
+                  <input type = "file" onChange = {this.handleChange}/>
+              </label>
+              <input type = "submit" value = "Submit"/>
+          </form>
+      )
+  }
+
+}
