6a5b705c6528afbfd916534c26e500407cc0a3f6
Jack-Saysana <saysanabjack@gmail.com>
2021-09-18 21:00:13 -0400

    Password Generation bby

--- Backend/routes/router.js
+++ Backend/routes/router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Jimp = require("jimp");
 
+let length = 8;
 /*
     Parameters:
         canvas - offscreen html canvas
@@ -17,11 +18,47 @@ router.post("/newImage", (req, res) => {
                 pixData.push(Jimp.intToRGBA(image.getPixelColor(i, j)));
             }
         }
-        res.send(pixData);
+        let passwordPool = genPasswordPool(pixData);
+        res.send(passwordPool);
     }).catch(err => {
         console.log(err);
-        res.send(err);
+        res.send({
+            status: 500,
+            message: "Input is not an image"
+        });
     });
 });
+
+router.post("/updateLength", (req, res) => {
+    length = req.body.length != null ? req.body.length : 8;
+    res.send(length);
+});
+
+const genPasswordPool = (pixData) => {
+    const GOLDEN_NUMBER = 17.09;
+    let bound = 80;
+
+    let chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                 "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+                 "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "(", ")", ".", "?", "[", "]", "-", "`", "~", ";", ":", "@", "#", "$", "%",
+                 "^", "&", "*", "+", "="];
+    let hash = [];
+    let passwordPool = [];
+
+    for(let pos = 0; pos <= bound + 1; pos++){
+        if(pos > bound){
+            chars.splice(Math.floor(Math.random() * bound), 1);
+            bound--;
+            pos = 0;
+        }
+        hash.push(chars[pos]);
+    }
+
+    for(let i = 0; i < pixData.length; i++){
+        pixVal = ((pixData[i].r / GOLDEN_NUMBER) * (pixData[i].b / GOLDEN_NUMBER) * (pixData[i].g / GOLDEN_NUMBER));
+        passwordPool.push(hash[Math.trunc(pixVal)]);
+    }
+    return passwordPool;
+}
 
 module.exports = router;
